9e49d6d47b8ecce6a5b4f1d704361879e04c5114
Colin Dean <colindean@us.ibm.com>
2017-02-13 17:29:14 -0500

    Removes erroneous copyrights

--- src/main/scala/example/App.scala
+++ src/main/scala/example/App.scala
@@ -1,18 +1,3 @@
-/**
-  * BEGIN_COPYRIGHT
-  *
-  * IBM Confidential
-  * OCO Source Materials
-  *
-  * 5725-Y07
-  * (C) Copyright IBM Corp. 2017. All Rights Reserved.
-  *
-  * The source code for this program is not published or otherwise
-  * divested of its trade secrets, irrespective of what has been
-  * deposited with the U.S. Copyright Office.
-  *
-  * END_COPYRIGHT
-  */
 package example
 import scala.concurrent.Await
 import scala.concurrent.duration._
